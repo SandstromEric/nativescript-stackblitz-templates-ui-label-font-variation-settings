7941d4a074598a95b7bc08c6e29fd83dbf1d0035
Eric Sandström <eric.wiljam.sandstrom@gmail.com>
2024-05-14 20:40:55 +0200

    - inital commit

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 
 import { Item } from './item'
 import { ItemService } from './item.service'
+import { Label } from '@nativescript/core'
 
 @Component({
   selector: 'ns-items',
@@ -9,10 +10,15 @@ import { ItemService } from './item.service'
 })
 export class ItemsComponent implements OnInit {
   items: Array<Item>
-
+  icon = String.fromCharCode(0xe838)
   constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
     this.items = this.itemService.getItems()
+  }
+
+  iconLoaded(event) {
+    const lbl: Label = event.object
+    lbl.style.fontVariationSettings = [{ axis: 'FILL', value: 1 }, { axis: 'wght', value: 700}, { axis: 'opsz', value: 48}]
   }
 }
